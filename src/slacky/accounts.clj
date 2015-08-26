18fb8434ef767c97eba6f9763817e1389a0611db
Oliver Hine <git@oliy.co.uk>
2015-08-26 18:31:44 +0000

    Less efficient, but more robust add-account

--- src/slacky/accounts.clj
+++ src/slacky/accounts.clj
@@ -2,19 +2,18 @@
   (:require [clojure.java.jdbc :as jdbc]
             [honeysql.core :as sql]))
 
-(defn add-account! [db token key]
-  (jdbc/with-db-transaction [db db]
-    (let [result (first (jdbc/insert! db :accounts {:token token
-                                                    :key key}))
-          account-id (get result (keyword "last_insert_rowid()"))]
-      (jdbc/insert! db :api_stats {:account_id account-id
-                                   :hits 0}))))
-
 (defn lookup-account [db token]
   (first (jdbc/query db (sql/format {:select [:*]
                                      :from [:accounts]
                                      :where [:= :token token]
                                      :limit 1}))))
+
+(defn add-account! [db token key]
+  (jdbc/with-db-transaction [db db]
+    (jdbc/insert! db :accounts {:token token
+                                :key key})
+    (jdbc/insert! db :api_stats {:account_id (:id (lookup-account db token))
+                                 :hits 0})))
 
 (defn api-hit! [db ^Number account-id]
   (let [command "UPDATE api_stats SET hits = hits + 1 WHERE account_id = ?"]
