dfd295fadf5a992c2e80328965e76bab8f984360
Oliver Hine <git@oliy.co.uk>
2015-09-09 21:31:16 +0000

    Small test improvement

--- test/slacky/integration_test.clj
+++ test/slacky/integration_test.clj
@@ -1,5 +1,6 @@
 (ns slacky.integration-test
   (:require [clj-http.client :as http]
+            [clojure.core.async :as a]
             [clojure.test :refer :all]
             [conjure.core :as cj]
             [slacky
@@ -29,39 +30,38 @@
                (cj/verify-called-once-with-args memecaptain/create-template "http://images.com/cat.jpg")
                (cj/verify-called-once-with-args memecaptain/create-instance template-id "cute cats" "FTW")))
 
-(deftest can-register-slack-account
+(deftest can-integrate-with-slack
   (let [token (clojure.string/replace (str (UUID/randomUUID)) "-" "")
         webhook-url "https://hooks.slack.com/services/foobarbaz"
         channel-name "my-channel"
         user-name "the-user"
-        slack-promise (promise)]
+        slack-messages (a/chan)]
 
     (cj/stubbing [memecaptain/create-template template-id
                   memecaptain/create-instance meme-url
                   google/image-search image-url
-                  slack/send-message (fn [webhook-url channel message]
-                                       (deliver slack-promise true))]
+                  slack/send-message (fn [& args]
+                                       (a/put! slack-messages args))]
 
-                 (is (= 200 (:status (http/post "http://localhost:8080/api/account"
-                                                {:throw-exceptions? false
-                                                 :form-params {:token token
-                                                               :key webhook-url}}))))
+                 (testing "can create an account"
+                   (is (= 200 (:status (http/post "http://localhost:8080/api/account"
+                                                  {:throw-exceptions? false
+                                                   :form-params {:token token
+                                                                 :key webhook-url}})))))
 
-                 (is (= "Your meme is on its way"
-                        (:body (http/post "http://localhost:8080/api/slack/meme"
-                                          {:throw-exceptions? false
-                                           :form-params {:token token
-                                                         :team_id "a"
-                                                         :team_domain "b"
-                                                         :channel_id "c"
-                                                         :channel_name channel-name
-                                                         :user_id "d"
-                                                         :user_name user-name
-                                                         :command "/meme"
-                                                         :text "cats | cute cats | FTW"}}))))
+                 (testing "can meme from a channel"
+                   (is (= "Your meme is on its way"
+                          (:body (http/post "http://localhost:8080/api/slack/meme"
+                                            {:throw-exceptions? false
+                                             :form-params {:token token
+                                                           :team_id "a"
+                                                           :team_domain "b"
+                                                           :channel_id "c"
+                                                           :channel_name channel-name
+                                                           :user_id "d"
+                                                           :user_name user-name
+                                                           :command "/meme"
+                                                           :text "cats | cute cats | FTW"}}))))
 
-                 (is (deref slack-promise 500 false))
-
-                 (cj/verify-called-once-with-args slack/send-message webhook-url (str "#" channel-name) (slack/meme-message user-name "cats | cute cats | FTW" meme-url))
-                 (cj/verify-called-once-with-args memecaptain/create-template "http://images.com/cat.jpg")
-                 (cj/verify-called-once-with-args memecaptain/create-instance template-id "cute cats" "FTW"))))
+                   (is (= [webhook-url (str "#" channel-name) (slack/meme-message user-name "cats | cute cats | FTW" meme-url)]
+                          (first (a/alts!! [slack-messages (a/timeout 500)]))))))))
