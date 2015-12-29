882d1d2164bdc009b73e63e18f6dad32ac306eaf
Oliver Hine <git@oliy.co.uk>
2015-12-29 17:42:07 +0000

    Implementing typeahead

--- resources/src/cljs/slacky/views/demo.cljs
+++ resources/src/cljs/slacky/views/demo.cljs
@@ -1,11 +1,12 @@
 (ns slacky.views.demo
   (:require [reagent.core :as r]
-            [ajax.core :refer [POST]]
+            [ajax.core :refer [POST GET]]
             [clojure.string :as string]
             [slacky.nav :refer [nav!]]))
 
 (def meme-input (r/atom nil))
 (def meme-output (r/atom nil))
+(def meme-patterns (r/atom []))
 
 (defn- classes [& classes]
   (string/join " " (remove nil? classes)))
@@ -40,6 +41,31 @@
                              ((nav! "/")))})
     ((nav! "/demo"))))
 
+(defn fetch-meme-patterns []
+  (GET "/api/meme/patterns"
+      {:response-format :json
+       :keywords? true
+       :handler #(reset! meme-patterns %)}))
+
+(def meme-typeahead
+  (with-meta
+    (fn []
+      [:div.tt-menu {:style {:position "absolute"
+                             :top "100%"
+                             :left 0
+                             :margin 0
+                             :z-index 100}}
+       [:div
+        (map (fn [{:keys [pattern template]}]
+               [:div.typeahead-result.tt-suggestion.tt-selectable
+                {:key (or template "no-template")
+                 :on-click #(reset! meme-input pattern)}
+                [:span pattern]
+                (when template
+                  [:div.hidden-xs
+                   [:img {:src template}]])]) @meme-patterns)]])
+    {:component-did-mount fetch-meme-patterns}))
+
 (defn- meme-form []
   [:div#demo.form-horizontal
    [:div.col-xs-12.col-md-11
@@ -48,14 +74,16 @@
       {:class (classes "input-group"
                        (when (= :error @meme-output) "has-error"))}
       [:div.input-group-addon "/meme"]
-      [:input#demo-text.form-control {:type "text"
-                                      :value @meme-input
-                                      :on-change #(reset! meme-input (-> % .-target .-value))
-                                      :on-key-down #(case (.-which %)
-                                                      13 (generate-meme)
-                                                      27 (reset! meme-input nil)
-                                                      nil)
-                                      :placeholder "search term or url | upper text | lower text"}]]]]
+      [:span
+       [:input#demo-text.form-control {:type "text"
+                                       :value @meme-input
+                                       :on-change #(reset! meme-input (-> % .-target .-value))
+                                       :on-key-down #(case (.-which %)
+                                                       13 (generate-meme)
+                                                       27 (reset! meme-input nil)
+                                                       nil)
+                                       :placeholder "search term or url | upper text | lower text"}]
+       [meme-typeahead]]]]]
    [:div.col-xs-12.col-md-1
     [:div.form-group.form-group-lg
      [:button.btn.btn-success.btn-lg
