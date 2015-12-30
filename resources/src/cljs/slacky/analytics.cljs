cc0ee7da92d46b0f013f70e73d5f97ccb3951892
Oliver Hine <git@oliy.co.uk>
2015-12-30 19:35:22 +0000

    Possibly fixed GA

--- resources/src/cljs/slacky/analytics.cljs
+++ resources/src/cljs/slacky/analytics.cljs
@@ -1,11 +1,7 @@
 (ns slacky.analytics)
 
 (defn event! [event-name]
-  (js/console.log js/window.ga)
-  (js/console.log js/ga)
-  (js/console.log js/document.ga)
-
-  (when (or js/window.ga js/ga js/document.ga)
+  (when js/ga
     (js/console.log "Recording event" event-name)
-    (let [r (.ga js/window "send" "event" event-name)]
+    (let [r (js/ga "send" "event" event-name)]
       (js/console.log "GA responded:" r))))
