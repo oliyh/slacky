57bc59ba8163ee6c95ce4b082e556a4e29cd9002
Oliver Hine <git@oliy.co.uk>
2015-12-30 19:02:41 +0000

    Debugging analytics

--- resources/src/cljs/slacky/analytics.cljs
+++ resources/src/cljs/slacky/analytics.cljs
@@ -1,5 +1,8 @@
 (ns slacky.analytics)
 
 (defn event! [event-name]
+  (js/console.log js/window.ga)
   (when js/window.ga
-    (.ga js/window "send" "event" event-name)))
+    (js/console.log "Recording event" event-name)
+    (let [r (.ga js/window "send" "event" event-name)]
+      (js/console.log "GA responded:" r))))
