943b3c86e45a84bd7055dcd42d1c3c43d48ff0b0
Oliver Hine <git@oliy.co.uk>
2015-12-30 19:31:16 +0000

    More GA debug

--- resources/src/cljs/slacky/analytics.cljs
+++ resources/src/cljs/slacky/analytics.cljs
@@ -2,7 +2,10 @@
 
 (defn event! [event-name]
   (js/console.log js/window.ga)
-  (when js/window.ga
+  (js/console.log js/ga)
+  (js/console.log js/document.ga)
+
+  (when (or js/window.ga js/ga js/document.ga)
     (js/console.log "Recording event" event-name)
     (let [r (.ga js/window "send" "event" event-name)]
       (js/console.log "GA responded:" r))))
