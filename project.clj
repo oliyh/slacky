ec8345b3d4c15822d9edd2c07b63f343be34bb42
Oliver Hine <git@oliy.co.uk>
2016-03-08 20:10:52 +0000

    Bumping pedestal-api

--- project.clj
+++ project.clj
@@ -4,9 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [pedestal-api "0.1.0-SNAPSHOT"]
-                 [io.pedestal/pedestal.service "0.4.0"]
-                 [io.pedestal/pedestal.jetty "0.4.0"]
+                 [org.clojure/core.async "0.2.374"]
+                 [pedestal-api "0.1.0"]
+                 [io.pedestal/pedestal.service "0.4.1"]
+                 [io.pedestal/pedestal.jetty "0.4.1"]
                  [angel-interceptor "0.2.0"]
 
                  [ch.qos.logback/logback-classic "1.1.2" :exclusions [org.slf4j/slf4j-api]]
