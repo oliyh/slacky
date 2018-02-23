1c0209ea2f71cfe12908f81e4a7f1543acd6ef19
Oliver Hine <git@oliy.co.uk>
2018-02-23 22:36:46 +0000

    building memecaptain into the right place

--- compile-memecaptain.sh
+++ compile-memecaptain.sh
@@ -4,4 +4,4 @@ curl -L http://enclose.io/rubyc/rubyc-linux-x64.gz | gunzip > rubyc
 chmod +x rubyc
 
 export CPPFLAGS="-P"
-./rubyc --gem=meme_captain --gem-version=0.3.1 bundle ./resources/memecaptain/memecaptain
+./rubyc --gem=meme_captain --gem-version=0.3.1 -o=./resources/memecaptain/memecaptain bundle
